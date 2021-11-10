e21d40a37b809d295e4e89ce4830b026a1d4e5d7
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-11-10 20:12:10 -0300

    add: client emit hello and receive welcome msg

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,15 @@
 const socket = io();
 socket.on('connect', () => {
   console.log('Connected to server');
+  socket.emit('createMessage', {
+    from: 'User',
+    text: 'Hello from client'
+  });
 });
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
+});
+
+socket.on('newMessage', (message) => {
+  console.log('New message', message);
 });
