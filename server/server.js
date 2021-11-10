1589322db5f122213b6aced94afe5e546fec7902
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-11-10 19:25:24 -0300

    add: http server and io config

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const path = require('path');
+const http = require('http');
 const express = require('express');
+const path = require('path');
 
-const publicPath = path.join(__dirname, '..', 'public');
+const socketIO = require('socket.io');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
+const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
