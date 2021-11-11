34c41a19a2d3d92d6061a7b3b0178c691d9a9b99
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-11-11 15:00:47 -0300

    add: client emit createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,7 @@
 const socket = io();
 socket.on('connect', () => {
   console.log('Connected to server');
-  // socket.emit('createMessage', {
-  //   from: 'User',
-  //   text: 'Hello from client'
-  // });
+  
 });
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
@@ -12,4 +9,11 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('New message', message);
+});
+
+socket.emit('createMessage', {
+    from: 'Lula',
+    text: '2022 to ai!'
+}, (data) => {
+  console.log(`${data} got it!`)
 });
