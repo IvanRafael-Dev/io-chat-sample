1d15edbb31087179d75613210f7de3b6be2e3ca6
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-11-10 21:40:53 -0300

    --amend

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 const socket = io();
 socket.on('connect', () => {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'User',
-    text: 'Hello from client'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'User',
+  //   text: 'Hello from client'
+  // });
 });
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
